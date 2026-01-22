505f9c651ed830bf36b9d51665b1f9a983ad7922
tharun790 <tharun790@gmail.com>
2026-01-22 17:22:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <button className="btn btn-primary mt-4">
+        DaisyUI Blue Button
+      </button>
+  );
+}
+
+export default App
